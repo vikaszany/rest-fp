8e770812c4f6b98c527139a693107f78ef914c25
Jain, Vikas @ CBRE Contractor <Vikas.Jain2@cbre.com>
2017-12-14 13:34:06 -0600

    third  part

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,18 @@
 import React from "react"
 import {render} from "react-dom"
 
+import {Header} from "../app/components/Header"
+import {Home} from  "../app/components/Home"
 class App extends React.Component{
     render(){
         return(
-            <div><h1>Hello hi!</h1>
+            <div className="container"> 
+            <div className="row"><Header/>
             </div>
+            <div><h1><Home/></h1>
+            </div>
+            </div>
+        
         );
 
     }
