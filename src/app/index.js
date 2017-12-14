4f68aedff8278bb5c15783d040161e2443571e09
Jain, Vikas @ CBRE Contractor <Vikas.Jain2@cbre.com>
2017-12-13 22:05:07 -0600

    second part

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("this is my react app");
+import React from "react"
+import {render} from "react-dom"
+
+class App extends React.Component{
+    render(){
+        return(
+            <div><h1>Hello hi!</h1>
+            </div>
+        );
+
+    }
+}
+
+render(<App/>, window.document.getElementById('app'));
