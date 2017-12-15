04cdca9c6a623526bd3942028280e459f33c791a
Jain, Vikas @ CBRE Contractor <Vikas.Jain2@cbre.com>
2017-12-14 20:51:36 -0600

    second part

--- src/app/index.js
+++ src/app/index.js
@@ -5,11 +5,18 @@ import {Header} from "../app/components/Header"
 import {Home} from  "../app/components/Home"
 class App extends React.Component{
     render(){
+        var users   = {
+            Name:"vikas",
+            Hobbies :["sports", "Heading"]
+        };
         return(
             <div className="container"> 
             <div className="row"><Header/>
             </div>
-            <div><h1><Home/></h1>
+            <div>
+                <Home name = {"vik"} age={27} user ={users} >
+              <p>this is awesome!</p>
+            </Home> 
             </div>
             </div>
         
